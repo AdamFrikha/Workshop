cc96e69b3e2bcda2bb154dab678f9421f1b0bef1
adamfrikha <adam.frikha@epitech.eu>
2021-03-04 11:41:11 +0100

    un vrai message

--- src/tool.c
+++ src/tool.c
@@ -35,6 +35,9 @@ static const char *STUDENT_SENTENCES[] = {
     "Y'aura un deuxieme BTTF ?",
     "J'ai un bug que j'arrive pas à résoudre, c'est urgent",
     "J'ai une question ?",
+    "bonjour, une phrase",
+    "rebonjour, deux phrases",
+    "re, trois phrases",
 };
 
 const char *get_sentence(size_t index)
